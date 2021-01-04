9af1f5dc2bc1d3bd35f72b2d1168e5fb7f26a635
Gonçalo <xicoffplays@gmail.com>
2021-01-04 00:03:14 +0000

    PetConfirme

--- SleepingElementsGame/Assets/scripts/Menus/Mainmenu.cs
+++ SleepingElementsGame/Assets/scripts/Menus/Mainmenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
+    public GameObject PetControl;
 
     public void PlayGame()
     {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        DontDestroyOnLoad(PetControl);
 
     }
 
